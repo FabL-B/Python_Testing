c31da22bf1a2da568a69ff6a62c893809134515f
FabL-B <errebelneibaf@gmail.com>
2024-12-09 11:04:34 +0100

    Minor correction in tests_points_limit file

--- tests/unit_tests/test_points_limit.py
+++ tests/unit_tests/test_points_limit.py
@@ -1,15 +1,15 @@
 import pytest
 
-from server import app
+from server import validate_club_points
 
 
 def test_valid_places_deduction():
     club = {'name': 'club test', 'points': '10'}
     places_required = 5
-    assert app.validate_club_points(club, places_required) is True
+    assert validate_club_points(club, places_required) is True
 
 def test_excessive_places_requested():
     club = {'name': 'club test', 'points': '10'}
     places_required = 15
     with pytest.raises(ValueError, match="Not enough points available"):
-        app.validate_club_points(club, places_required)
+        validate_club_points(club, places_required)

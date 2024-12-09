69e829b92e469732300f103695bfa99eecd1165f
FabL-B <errebelneibaf@gmail.com>
2024-12-09 17:00:30 +0100

    Updated unit tests to use MAX_PLACES_PER_COMPETITION global variable.

--- tests/unit_tests/test_validate_max_places.py
+++ tests/unit_tests/test_validate_max_places.py
@@ -1,15 +1,16 @@
 import pytest
 
-from server import validate_max_places
+from server import validate_max_places, MAX_PLACES_PER_COMPETITION
 
 
 def test_valid_max_places():
-    places_requested = 10
-    max_places = 12
-    assert validate_max_places(places_requested, max_places) is True
+    places_required = 10
+    assert validate_max_places(places_required) is True
 
 def test_excessive_max_places():
-    places_requested = 20
-    max_places = 12
-    with pytest.raises(ValueError, match=f"Cannot book more than {max_places} places."):
-        validate_max_places(places_requested, max_places)
+    places_required = 20
+    error_message = (
+        f"Cannot book more than {MAX_PLACES_PER_COMPETITION} places."
+    )
+    with pytest.raises(ValueError, match=error_message):
+        validate_max_places(places_required)

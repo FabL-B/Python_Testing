50e2d851fe3bf5b8c8e8a22d3ad5639532c9d85d
FabL-B <errebelneibaf@gmail.com>
2024-12-20 11:12:37 +0100

    added test for logout route

---
+++ tests/unit_tests/test_logout.py
@@ -0,0 +1,19 @@
+import pytest
+from server import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_logout_route(client):
+    """Test the logout route."""
+    response = client.get('/logout')
+    
+    assert response.status_code == 302 
+    assert response.headers["Location"] == "/" 
+
+    follow_response = client.get(response.headers["Location"])
+    assert follow_response.status_code == 200
